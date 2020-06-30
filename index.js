be51ded19bfc9128962a6e50fea56e8ad0904731
ldj <540130049@qq.com>
2020-06-30 15:36:23 +0800

    初始化login路由

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+const cors = require('cors')
+const loginRouter = require(path.join(__dirname, 'router/login.js'))
+
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use('/api', loginRouter)
 
 app.listen(3000, () => {
     console.log('running...');
